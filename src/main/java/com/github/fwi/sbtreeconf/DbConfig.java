06089089f4825e069c43e128519593af0221752f
Frederik Wiers <fwi@github.com>
2022-02-05 00:52:18 +0100

    Add missing configuration annotation

--- src/main/java/com/github/fwi/sbtreeconf/DbConfig.java
+++ src/main/java/com/github/fwi/sbtreeconf/DbConfig.java
@@ -3,8 +3,10 @@ package com.github.fwi.sbtreeconf;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
 @Import({ 
 	DataSourceAutoConfiguration.class, 
 	DataSourceTransactionManagerAutoConfiguration.class,
